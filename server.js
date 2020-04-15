5be0a6d333095c9acab0c083f64c14551a4a1aca
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-04-15 16:57:04 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://heroku_mt4sb07g:jiawen96@ds139960.mlab.com:39960/heroku_mt4sb07g', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -15,7 +15,12 @@ app.use(function (req, res, next) {
     next();
 });
 
-require('./controllers/quiz.controller.server')(app)
-require('./controllers/question.controller.server')(app)
+require('./controllers/quiz.controller.server')(app);
+require('./controllers/question.controller.server')(app);
+
+const bodyParser = require( 'body-parser' );
+app.use ( bodyParser.urlencoded ({ extended : false }));
+app.use ( bodyParser.json ());
+require('./controllers/quiz-attempts.controller.server')(app)
 
 app.listen(process.env.PORT || 3000)
